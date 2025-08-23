9e2463243ee77aef1033f1fab563126323504006
MD-22 <manav2004duseja@gmail.com>
2025-08-24 01:47:47 +0530

    adding random string

--- secret language.py
+++ secret language.py
@@ -1,10 +1,23 @@
 #secret language project
 #Data=Manav
 #encyrption=jcbanavMned
+import random
+import string
+result=""
+for i in range(3):
+    result+=random.choice(string.ascii_letters)
+result1=""
+for i in range(3):
+    result1+=random.choice(string.ascii_letters)    
+#print(result) 
+
+
 n=input("enter ur data:- ")
 if len(n)<=3:
     a=n[::-1]
     print(a)
+    print(f"{result}{a}{result1}")
+    
 else:
-    print(n[0])
-    print (n[1:len(n)]+n[0])
+    #print(n[0])
+    print(f"{result}{n[1:len(n)]+n[0]}{result1}")
