46a5a8954bad4cf52efa110313b0810ca49c9ab4
MD-22 <manav2004duseja@gmail.com>
2025-08-24 02:27:32 +0530

    add decryption

--- secret language.py
+++ secret language.py
@@ -1,8 +1,12 @@
 #secret language project
 #Data=Manav
 #encyrption=jcbanavMned
-import random
+
+
+#encryption
+'''import random
 import string
+
 result=""
 for i in range(3):
     result+=random.choice(string.ascii_letters)
@@ -10,7 +14,6 @@ result1=""
 for i in range(3):
     result1+=random.choice(string.ascii_letters)    
 #print(result) 
-
 
 n=input("enter ur data:- ")
 if len(n)<=3:
@@ -20,4 +23,19 @@ if len(n)<=3:
     
 else:
     #print(n[0])
-    print(f"{result}{n[1:len(n)]+n[0]}{result1}")
+    print(f"{result}{n[1:len(n)]+n[0]}{result1}")'''
+
+
+#Now decryption
+
+n=input("enter ur data:- ")
+if len(n)<=9:
+    a=n[-4:2:-1]
+    print(a)
+    
+else:
+    b=n[3:-3]
+    print(b)
+    print(f"{b[-1]}{b[0:-1]}")
+
+   
